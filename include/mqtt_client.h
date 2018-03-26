21633799e3658d387a3b7750c4344d929439db48
XuanZe <119524428@qq.com>
2018-03-26 15:44:43 +0800

    添加头文件说明框架

--- include/mqtt_client.h
+++ include/mqtt_client.h
@@ -4,9 +4,14 @@
  * Tuan PM <tuanpm at live dot com>
  */
 
-#ifndef _MQTT_CLIENT_H_
-#define _MQTT_CLIENT_H_
-
+#pragma once
+/**
+ * @file mqtt_client.h
+ *
+ * //TODO:添加头文件说明
+ * This header file defines interface between PM lock functions (pm_locks.c)
+ * and the chip-specific power management (DFS/light sleep) implementation.
+ */
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
@@ -16,6 +21,11 @@
 
 typedef struct esp_mqtt_client* esp_mqtt_client_handle_t;
 
+/*
+ * Enumeration for hash operations.
+ * When WPA2 is connecting, this enum is used to
+ * request a hash algorithm via crypto_hash_xxx functions.
+ */
 typedef enum {
     MQTT_EVENT_ERROR = 0,
     MQTT_EVENT_CONNECTED,
@@ -26,6 +36,11 @@ typedef enum {
     MQTT_EVENT_DATA,
 } esp_mqtt_event_id_t;
 
+/*
+ * Enumeration for hash operations.
+ * When WPA2 is connecting, this enum is used to
+ * request a hash algorithm via crypto_hash_xxx functions.
+ */
 typedef enum {
     MQTT_TRANSPORT_UNKNOWN = 0x0,
     MQTT_TRANSPORT_OVER_TCP,
@@ -33,6 +48,7 @@ typedef enum {
     MQTT_TRANSPORT_OVER_WS,
     MQTT_TRANSPORT_OVER_WSS
 } esp_mqtt_transport_t;
+
 
 typedef struct {
     esp_mqtt_event_id_t event_id;
@@ -47,11 +63,25 @@ typedef struct {
     int msg_id;
 } esp_mqtt_event_t;
 
+/*
+ * This structure is about the algorithm when do crypto_hash operation, for detail,
+ * please reference to the structure crypto_hash.
+ */
 typedef esp_mqtt_event_t* esp_mqtt_event_handle_t;
 
+/**
+  * @brief The crypto callback function used in wpa enterprise hash operation when connect.
+  *        Initialize a esp_crypto_hash_t structure.
+  *
+  * @param alg  Hash algorithm.
+  *
+  */
 typedef esp_err_t (* mqtt_event_callback_t)(esp_mqtt_event_handle_t event);
 
-
+/*
+ * This structure is about the algorithm when do crypto_cipher operation, for detail,
+ * please reference to the structure crypto_cipher.
+ */
 typedef struct {
     mqtt_event_callback_t event_handle;
     char host[MQTT_MAX_HOST_LEN];
@@ -76,13 +106,53 @@ typedef struct {
     esp_mqtt_transport_t transport;
 } esp_mqtt_client_config_t;
 
+/**
+ * @brief
+ * @param
+ * @return
+ */
 esp_mqtt_client_handle_t esp_mqtt_client_init(const esp_mqtt_client_config_t *config);
+/**
+ * @brief
+ * @param
+ * @return
+ */
 esp_err_t esp_mqtt_client_set_uri(esp_mqtt_client_handle_t client, const char *uri);
+/**
+ * @brief
+ * @param
+ * @return
+ */
 esp_err_t esp_mqtt_client_start(esp_mqtt_client_handle_t client);
+/**
+ * @brief
+ * @param
+ * @return
+ */
 esp_err_t esp_mqtt_client_stop(esp_mqtt_client_handle_t client);
+/**
+ * @brief
+ * @param
+ * @return
+ */
 esp_err_t esp_mqtt_client_subscribe(esp_mqtt_client_handle_t client, const char *topic, int qos);
+/**
+ * @brief
+ * @param
+ * @return
+ */
 esp_err_t esp_mqtt_client_unsubscribe(esp_mqtt_client_handle_t client, const char *topic);
+/**
+ * @brief
+ * @param
+ * @return
+ */
 int esp_mqtt_client_publish(esp_mqtt_client_handle_t client, const char *topic, const char *data, int len, int qos, int retain);
+/**
+ * @brief
+ * @param
+ * @return
+ */
 esp_err_t esp_mqtt_client_destroy(esp_mqtt_client_handle_t client);
 
 #endif
